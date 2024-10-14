49d4abfd6aa6ef10bf3cbaa19ea7109caa7308f3
lucas8790 <lv8790121@gmail.com>
2024-10-14 14:06:59 -0300

    Registrado usuário no sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController{
     static async register(req, res){
@@ -28,6 +29,39 @@ module.exports = class UserController{
         if(password !== confirmpassword){
             res.status(422).json({ message: 'A senha e a confirmação da senha precisam ser iguais'})
             return
+        }
+
+        // Check if user exists
+        const userExists = await User.findOne({ email: email})
+
+        if(userExists){
+            res.status(422).json({
+                message: 'Por favor, utilize outro e-mail',
+            })
+            return
+        
+        }
+
+        // Create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // Create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash,
+        })
+
+        try{
+            const newUser = await user.save()
+            res.status(201).json({
+                message: 'Usuário Criado',
+                newUser,
+            })
+        } catch(error){
+            res.status(500).json({message: error})
         }
     }
 }
