a9d6ed17b67bafd5b640b6d0375da4ce81299ce8
lucas8790 <lv8790121@gmail.com>
2024-10-14 13:59:00 -0300

    Criação de algumas validações

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,32 @@ const User = require('../models/User')
 
 module.exports = class UserController{
     static async register(req, res){
-        res.json('Ola Get A Pet')
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        // Validations
+        if(!name){
+            res.status(422).json({ message: 'O nome é obrigatório'})
+            return
+        }
+        if(!email){
+            res.status(422).json({ message: 'O email é obrigatório'})
+            return
+        }
+        if(!phone){
+            res.status(422).json({ message: 'O telefone é obrigatório'})
+            return
+        }
+        if(!password){
+            res.status(422).json({ message: 'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword){
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória'})
+            return
+        }
+        if(password !== confirmpassword){
+            res.status(422).json({ message: 'A senha e a confirmação da senha precisam ser iguais'})
+            return
+        }
     }
 }
