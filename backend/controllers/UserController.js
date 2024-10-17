433941a0b3e179693cc86fcd753ad2d8b617c26a
lucas8790 <lv8790121@gmail.com>
2024-10-16 21:01:16 -0300

    Validações para atualização de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 // Helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController{
     static async register(req, res){
@@ -131,9 +132,49 @@ module.exports = class UserController{
     }
 
     static async editUser(req, res){
-        res.status(200).json({
-            message: 'Deu certo update!',
-        })
-        return
+        const id = req.params.id
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const {name, email, phone, password, confirmpassword} = req.body
+        let image = ''
+
+        // Validações
+        if(!name){
+            res.status(422).json({ message: 'O nome é obrigatório'})
+            return
+        }
+        if(!email){
+            res.status(422).json({ message: 'O email é obrigatório'})
+            return
+        }
+
+        const userExists = await User.findOne({ email: email })
+        if(user.enail !== email && userExists){
+            res.status(422).json({
+                message: 'Por favor, utilize outro e-mail!',
+            })
+            return
+        }
+
+        user.email = email
+
+        if(!phone){
+            res.status(422).json({ message: 'O telefone é obrigatório'})
+            return
+        }
+        if(!password){
+            res.status(422).json({ message: 'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword){
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória'})
+            return
+        }
+        if(password !== confirmpassword){
+            res.status(422).json({ message: 'A senha e a confirmação da senha precisam ser iguais'})
+            return
+        }
     }
 }
